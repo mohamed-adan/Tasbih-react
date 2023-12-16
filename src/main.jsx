23e83c60bb239d43bc8886b9738baab83676ae78
mohamed-adan <mohammedomaraadan@gmail.com>
2023-12-16 10:53:11 +0300

    this is tasbiih

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Tasbih from './Tasbih'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  
+  <Tasbih/>
+
+
+
+ 
+)
